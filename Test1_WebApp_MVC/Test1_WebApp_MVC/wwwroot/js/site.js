d231c2f8507074550ee9d9c4201617af0c7bd668
moonica <jonicabrown@viewcentre.co.uk>
2024-03-13 15:51:33 +0200

    Add JS confirmation popup before deleting

--- Test1_WebApp_MVC/Test1_WebApp_MVC/wwwroot/js/site.js
+++ Test1_WebApp_MVC/Test1_WebApp_MVC/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿'use strict';
 
-// Write your JavaScript code.
+//TODO: use document.ready
+
+function confirmDelete(userId, path) {
+    if (confirm(`Are you sure you want to delete user ${userId}?`)) {
+        location.href = path;
+    }
+}
